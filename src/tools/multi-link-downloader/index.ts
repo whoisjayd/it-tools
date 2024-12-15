f8aa7db20a8f728f4e01e0a4f82d1ab3561a8d9e
ShareVB <sharevb@gmail.com>
2024-12-15 19:07:18 +0100

    fix(Multilink Downloader): spelling

--- src/tools/multi-link-downloader/index.ts
+++ src/tools/multi-link-downloader/index.ts
@@ -4,7 +4,7 @@ import { defineTool } from '../tool';
 export const tool = defineTool({
   name: 'Multi link downloader',
   path: '/multi-link-downloader',
-  description: '',
+  description: 'The tool will asynchronously combine the contents of the provided links into a single zip file for you to download. (Requires an internet connection)',
   keywords: ['multi', 'link', 'downloader'],
   component: () => import('./multi-link-downloader.vue'),
   icon: IconFileDownload,
