0852d1822f5249897ac235744fcdfcdfd9d69f5f
derekcentrico <derek@derekgordon.com>
2025-01-29 15:40:04 -0500

    added WPA2/WPA3 and WPA3 only.
    added WPA2/WPA3 and WPA3 only.

--- src/tools/wifi-qr-code-generator/wifi-qr-code-generator.vue
+++ src/tools/wifi-qr-code-generator/wifi-qr-code-generator.vue
@@ -53,6 +53,14 @@ const { copy } = useCopy({ source: text, text: 'Copied to the clipboard' });
               value: 'WPA',
             },
             {
+              label: 'WPA3-only (No WPA2 fallback)',
+              value: 'WPA3',
+            },
+            {
+              label: 'WPA2/WPA3 (Mixed Mode)',
+              value: 'WPA2/WPA3',
+            },
+            {
               label: 'WEP',
               value: 'WEP',
             },
