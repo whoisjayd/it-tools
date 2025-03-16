76c28040524aa4222167f7913c5d746147e54a8f
ShareVB <sharevb@gmail.com>
2025-03-16 22:50:20 +0100

    feat(Passphrase Generator): add option to add number at end of some words
    Fix https://github.com/CorentinTh/it-tools/discussions/1228#discussioncomment-12514695

--- src/tools/passphrase-generator/passphrase-generator.vue
+++ src/tools/passphrase-generator/passphrase-generator.vue
@@ -3,20 +3,45 @@ import { generateSillyPassword } from 'silly-password-generator';
 import { useCopy } from '@/composable/copy';
 import { useQueryParamOrStorage } from '@/composable/queryParams';
 import { computedRefreshable } from '@/composable/computedRefreshable';
+import { randIntFromInterval } from '@/utils/random';
 
 const count = useQueryParamOrStorage({ name: 'count', storageName: 'pass-generator:count', defaultValue: 1 });
 const words = useQueryParamOrStorage({ name: 'words', storageName: 'pass-generator:words', defaultValue: 5 });
+const numbers = useQueryParamOrStorage({ name: 'nums', storageName: 'pass-generator:nums', defaultValue: 0 });
 const capitalize = useQueryParamOrStorage({ name: 'capitalize', storageName: 'pass-generator:capitalize', defaultValue: false });
 const saltChars = useQueryParamOrStorage({ name: 'salt', storageName: 'pass-generator:salt', defaultValue: '' });
 const separator = useQueryParamOrStorage({ name: 'sep', storageName: 'pass-generator:sep', defaultValue: '-' });
 
-const [passphrases, refreshPassphrases] = computedRefreshable(() =>
-  Array.from({ length: count.value },
-    () => generateSillyPassword({
-      capitalize: capitalize.value,
-      wordCount: words.value,
-      salt: saltChars.value,
-    }).replace(/\s+/g, separator.value)).join('\n'),
+const [passphrases, refreshPassphrases] = computedRefreshable(
+  () => Array.from({ length: count.value },
+    () => {
+      const requiredNumbers = Math.min(words.value, numbers.value);
+      const nums: number[] = [];
+      if (words.value === requiredNumbers) {
+        for (let i = 0; i < requiredNumbers; i++) {
+          nums.push(i);
+        }
+      }
+      else {
+        while (nums.length < requiredNumbers) {
+          const rndNumber = randIntFromInterval(0, words.value - 1);
+          if (!nums.includes(rndNumber)) {
+            nums.push(rndNumber);
+          }
+        }
+      }
+
+      return generateSillyPassword({
+        capitalize: capitalize.value,
+        wordCount: words.value,
+        salt: saltChars.value,
+      }).split(/\s+/g).map((word, i) => {
+        if (nums.includes(i)) {
+          return word + randIntFromInterval(1, 100);
+        }
+        return word;
+      }).join(separator.value);
+    }).join('\n'),
 );
 
 const { copy } = useCopy({ source: passphrases, text: 'Passphrase(s) copied to clipboard!' });
@@ -28,6 +53,10 @@ const { copy } = useCopy({ source: passphrases, text: 'Passphrase(s) copied to c
       <n-form-item :label="`Words (${words})`" label-placement="left">
         <n-slider v-model:value="words" :step="1" :min="1" :max="512" mr-2 />
         <n-input-number v-model:value="words" size="small" />
+      </n-form-item>
+      <n-form-item label="Add numbers after X words" label-placement="left">
+        <n-slider v-model:value="numbers" :step="1" :min="1" :max="words" mr-2 />
+        <n-input-number v-model:value="numbers" :max="words" size="small" />
       </n-form-item>
 
       <n-space>
