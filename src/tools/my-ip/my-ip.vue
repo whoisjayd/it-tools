f3814e9a5ba8236ca029bfdc5b5a577aecb1aac7
ShareVB <sharevb@gmail.com>
2025-03-17 23:19:14 +0100

    feat(My IP): show IPv4, IPv6 and country
    Fix #1535

--- src/tools/my-ip/my-ip.vue
+++ src/tools/my-ip/my-ip.vue
@@ -1,42 +1,85 @@
 <script setup lang="ts">
+import { useScriptTag } from '@vueuse/core';
 import { computedRefreshableAsync } from '@/composable/computedRefreshable';
-import { useCopy } from '@/composable/copy';
 
-const [clientIP, refreshClientIP] = computedRefreshableAsync(async () => {
+declare global {
+  interface Window {
+    IpLookup: (ip: string) => Promise<{
+      country: string
+      country_name: string
+      country_native: string
+      continent: string
+      continent_name: string
+
+    }>
+  }
+}
+
+const { load: loadIpLookup } = useScriptTag('/iplookup.js', undefined, { type: 'module', manual: true });
+
+const [clientIPDetails, refreshClientIP] = computedRefreshableAsync(async () => {
+  const ipv4 = { ip: '', error: '' };
+  const ipv6 = { ip: '', error: '' };
+  const location = {
+    country: {
+      country: '',
+      country_name: '',
+      country_native: '',
+      continent: '',
+      continent_name: '',
+    },
+    error: '',
+  };
   try {
-    return (await (await fetch('https://api64.ipify.org?format=json', {
+    ipv4.ip = (await (await fetch('https://api4.ipify.org?format=json', {
       mode: 'cors',
     })).json()).ip?.toString();
   }
   catch (e: any) {
-    return e.toString();
+    ipv4.error = `Not detected (${e.toString()})`;
   }
-});
+  try {
+    ipv6.ip = (await (await fetch('https://api6.ipify.org?format=json', {
+      mode: 'cors',
+    })).json()).ip?.toString();
+  }
+  catch (e: any) {
+    ipv6.error = `Not detected (${e.toString()})`;
+  }
 
-const { copy } = useCopy({ source: clientIP, text: 'Client IP copied to the clipboard' });
+  await loadIpLookup();
+  try {
+    location.country = await window.IpLookup(ipv6.ip || ipv4.ip);
+  }
+  catch (e: any) {
+    location.error = e.toString();
+  }
+
+  return {
+    ipv4: ipv4.ip || ipv4.error,
+    ipv6: ipv6.ip || ipv6.error,
+    location: {
+      country: location.country?.country || `ERROR: ${location.error || 'Unknown'}`,
+      country_name: location.country ? `${location.country?.country_name}/${location.country?.country_native}` : '',
+      continent: location.country ? location.country?.continent : '',
+    },
+  };
+});
 </script>
 
 <template>
-  <c-card title="Your IPv4/6 address">
-    <div class="ip">
-      {{ clientIP }}
+  <c-card title="Your IPv4/6 address details">
+    <div v-if="clientIPDetails">
+      <input-copyable v-model:value="clientIPDetails.ipv4" label-position="left" label-width="100px" label-align="right" readonly label="IPv4:" placeholder="Your IPv4" />
+      <input-copyable v-model:value="clientIPDetails.ipv6" label-position="left" label-width="100px" label-align="right" readonly label="IPv6:" placeholder="Your IPv6" />
+      <input-copyable v-model:value="clientIPDetails.location.country" label-position="left" label-width="100px" label-align="right" readonly label="Country:" />
+      <input-copyable v-model:value="clientIPDetails.location.country_name" label-position="left" label-width="100px" label-align="right" readonly label="Country Name:" />
+      <input-copyable v-model:value="clientIPDetails.location.continent" label-position="left" label-width="100px" label-align="right" readonly label="Continent Name:" />
     </div>
     <div flex justify-center gap-3>
-      <c-button @click="copy()">
-        Copy
-      </c-button>
       <c-button @click="refreshClientIP">
         Refresh
       </c-button>
     </div>
   </c-card>
 </template>
-
-<style lang="less" scoped>
-.ip {
-  text-align: center;
-  font-size: 26px;
-  font-weight: 400;
-  margin: 10px 0 25px;
-}
-</style>
