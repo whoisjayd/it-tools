33cffc5d73c816af69cbf33f2316dbe7a3d20c79
ShareVB <sharevb@gmail.com>
2025-04-20 23:12:33 +0200

    feat(Text Diff): add word wrap option
    Fix #1554

--- src/tools/text-diff/text-diff.vue
+++ src/tools/text-diff/text-diff.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+const wordWrap = ref(false);
+</script>
+
 <template>
-  <c-card w-full important:flex-1 important:pa-0>
-    <c-diff-editor />
-  </c-card>
+  <div w-full>
+    <n-space justify="center" mb-3>
+      <n-checkbox v-model:checked="wordWrap">
+        Word Wrap?
+      </n-checkbox>
+    </n-space>
+    <c-card w-full important:flex-1 important:pa-0>
+      <c-diff-editor :options="{ wordWrap: wordWrap ? 'on' : 'off' }" />
+    </c-card>
+  </div>
 </template>
