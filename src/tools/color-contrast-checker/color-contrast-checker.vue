ad213e082dde976c157f6a18176600ebeb5ce159
ShareVB <sharevb@gmail.com>
2025-04-20 20:12:41 +0200

    feat(Color Contrast Checker): enhanced UI
    Fix https://github.com/sharevb/it-tools/issues/48#issuecomment-2786796909

--- src/tools/color-contrast-checker/color-contrast-checker.vue
+++ src/tools/color-contrast-checker/color-contrast-checker.vue
@@ -1,25 +1,49 @@
 <script setup lang="ts">
-import { contrastRatio, passesWCAG } from 'wcag-contrast-utils';
+import { apcaContrastValue, hex, wcag, wcagContrastValue } from 'a11y-color-contrast';
 import { useQueryParam } from '@/composable/queryParams';
 
 const forecolor = useQueryParam({ name: 'fg', defaultValue: '#000' });
 const backcolor = useQueryParam({ name: 'bg', defaultValue: '#fff' });
 
+const levels = [
+  { score: 3, text: 'Very poor', bg: '#fbd0da', fg: '#5f071c', stars: 1 },
+  { score: 4.5, text: 'Poor', bg: '#fbd0da', fg: '#5f071c', stars: 2 },
+  { score: 7, text: 'Good', bg: '#fbf5d0', fg: '#5f5207', stars: 3 },
+  { score: 12, text: 'Very good', bg: '#d2fbd0', fg: '#0d5f07', stars: 4 },
+  { score: 9999, text: 'Super', bg: '#d2fbd0', fg: '#0d5f07', stars: 5 },
+];
+const smallLevels = [
+  { score: 4.5, bg: '#fbd0da', fg: '#5f071c', stars: 1 },
+  { score: 7, bg: '#fbf5d0', fg: '#5f5207', stars: 2 },
+  { score: 9999, bg: '#d2fbd0', fg: '#0d5f07', stars: 3 },
+];
+const largeLevels = [
+  { score: 3, bg: '#fbd0da', fg: '#5f071c', stars: 1 },
+  { score: 4.5, bg: '#fbf5d0', fg: '#5f5207', stars: 2 },
+  { score: 9999, bg: '#d2fbd0', fg: '#0d5f07', stars: 3 },
+];
+
 const result = computed(() => {
   try {
-    const constrastValue = contrastRatio(forecolor.value, backcolor.value);
-    const wcagLevel = passesWCAG(constrastValue);
+    const fgHex = hex(forecolor.value);
+    const bgHex = hex(backcolor.value);
 
+    const wcagLevel = wcagContrastValue(bgHex, fgHex);
     return {
-      constrastValue,
-      wcagLevel,
+      wcag21ConstrastValue: wcagLevel,
+      aaNormal: wcag(bgHex, fgHex, { level: 'AA', size: 'normal' }),
+      aaaNormal: wcag(bgHex, fgHex, { level: 'AAA', size: 'normal' }),
+      aaLarge: wcag(bgHex, fgHex, { level: 'AA', size: 'large' }),
+      aaaLarge: wcag(bgHex, fgHex, { level: 'AAA', size: 'large' }),
+      level: levels.find(l => wcagLevel < l.score),
+      largeLevel: largeLevels.find(l => wcagLevel < l.score),
+      smallLevel: smallLevels.find(l => wcagLevel < l.score),
+      apcaContrastValue: apcaContrastValue(bgHex, fgHex),
       error: '',
     };
   }
   catch (e: any) {
     return {
-      constrastValue: -1,
-      wcagLevel: '',
       error: e.toString(),
     };
   }
@@ -57,7 +81,57 @@ const result = computed(() => {
       {{ result.error }}
     </c-alert>
 
-    <input-copyable v-if="!result.error" readonly label="Constrast:" label-width="100px" label-position="left" :value="result.constrastValue" mb-2 />
-    <input-copyable v-if="!result.error" readonly label="Level:" label-width="100px" label-position="left" :value="result.wcagLevel" />
+    <c-card v-if="!result.error" title="WCAG 2.1">
+      <input-copyable readonly label="WCAG Constrast:" label-width="130px" label-position="left" :value="result.wcag21ConstrastValue" mb-3 />
+
+      <n-form-item v-if="result.level" label="Contrast quality:" label-width="130px" label-placement="left">
+        <n-tag :max="5" :color="{ color: result.level.bg, textColor: result.level.fg, borderColor: result.level.fg }" mr-2>
+          <span>{{ result.wcag21ConstrastValue.toFixed(2) }} - {{ result.level.text }}</span>
+        </n-tag>
+        <n-tag :max="5" :color="{ color: result.level.bg, textColor: result.level.fg, borderColor: result.level.fg }">
+          <n-rate :count="5" readonly :default-value="result.level.stars" :color="result.level.fg" />
+        </n-tag>
+      </n-form-item>
+      <n-form-item v-if="result.smallLevel" label="Small texts:" label-width="130px" label-placement="left">
+        <n-tag :color="{ color: result.smallLevel.bg, textColor: result.smallLevel.fg, borderColor: result.smallLevel.fg }">
+          <n-rate :count="3" readonly :default-value="result.smallLevel.stars" :color="result.smallLevel.fg" />
+        </n-tag>
+      </n-form-item>
+      <n-form-item v-if="result.largeLevel" label="Large texts:" label-width="130px" label-placement="left">
+        <n-tag :color="{ color: result.largeLevel.bg, textColor: result.largeLevel.fg, borderColor: result.largeLevel.fg }">
+          <n-rate :count="3" readonly :default-value="result.largeLevel.stars" :color="result.largeLevel.fg" />
+        </n-tag>
+      </n-form-item>
+
+      <c-card title="Normal text">
+        <n-form-item label="WCAG 2.1 AA:" label-width="130px" label-placement="left">
+          <n-tag :bordered="false" :type="result.aaNormal ? 'success' : 'error'">
+            {{ result.aaNormal ? 'Pass' : 'Fail' }}
+          </n-tag>
+        </n-form-item>
+        <n-form-item label="WCAG 2.1 AAA:" label-width="130px" label-placement="left">
+          <n-tag :bordered="false" :type="result.aaaNormal ? 'success' : 'error'">
+            {{ result.aaaNormal ? 'Pass' : 'Fail' }}
+          </n-tag>
+        </n-form-item>
+      </c-card>
+      <c-card title="Large text">
+        <n-p><i>Text that is 14pt (18.67px) and bold or any text bigger than 18pt (24px) is defined as large scale text.</i></n-p>
+        <n-form-item label="WCAG 2.1 AA:" label-width="130px" label-placement="left">
+          <n-tag :bordered="false" :type="result.aaLarge ? 'success' : 'error'">
+            {{ result.aaLarge ? 'Pass' : 'Fail' }}
+          </n-tag>
+        </n-form-item>
+        <n-form-item label="WCAG 2.1 AAA:" label-width="130px" label-placement="left">
+          <n-tag :bordered="false" :type="result.aaaLarge ? 'success' : 'error'">
+            {{ result.aaaLarge ? 'Pass' : 'Fail' }}
+          </n-tag>
+        </n-form-item>
+      </c-card>
+    </c-card>
+
+    <c-card v-if="!result.error" title="APCA">
+      <input-copyable readonly label="APCA Constrast:" label-width="130px" label-position="left" :value="result.apcaContrastValue" mb-2 />
+    </c-card>
   </div>
 </template>
