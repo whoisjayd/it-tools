60475933b4f29b7d9997c1d4af11c912e8a48e88
ShareVB <sharevb@gmail.com>
2025-04-21 23:01:50 +0200

    fix(JSON Prettifier): ui

--- src/tools/json-viewer/json-viewer.vue
+++ src/tools/json-viewer/json-viewer.vue
@@ -35,22 +35,20 @@ const rawJsonValidation = useValidation({
 </script>
 
 <template>
-  <div style="flex: 0 0 100%">
-    <div style="margin: 0 auto; max-width: 600px" flex justify-center gap-3>
-      <n-form-item label="Sort keys :" label-placement="left" label-width="100">
-        <n-switch v-model:value="sortKeys" />
-      </n-form-item>
-      <n-form-item label="Unescape Unicode :" label-placement="left" label-width="150">
-        <n-switch v-model:value="unescapeUnicode" />
-      </n-form-item>
-      <n-form-item label="Indent size :" label-placement="left" label-width="100" :show-feedback="false">
-        <n-input-number v-model:value="indentSize" min="0" max="10" style="width: 100px" />
-      </n-form-item>
-      <n-form-item :label="`${repairJsonLabel} :`" label-placement="left" label-width="110">
-        <n-switch v-model:value="repairJson" />
-      </n-form-item>
-    </div>
-  </div>
+  <n-space justify="center">
+    <n-form-item label="Sort keys :" label-placement="left" label-width="100">
+      <n-switch v-model:value="sortKeys" />
+    </n-form-item>
+    <n-form-item label="Unescape Unicode :" label-placement="left" label-width="150">
+      <n-switch v-model:value="unescapeUnicode" />
+    </n-form-item>
+    <n-form-item label="Indent size :" label-placement="left" label-width="100" :show-feedback="false">
+      <n-input-number v-model:value="indentSize" min="0" max="10" style="width: 100px" />
+    </n-form-item>
+    <n-form-item :label="`${repairJsonLabel} :`" label-placement="left" label-width="110">
+      <n-switch v-model:value="repairJson" />
+    </n-form-item>
+  </n-space>
 
   <n-form-item
     label="Your raw JSON"
