5e3648e1410d91df61ac8c99fa380b6c013f50f9
ShareVB <sharevb@gmail.com>
2025-07-06 12:53:17 +0200

    feat(HTML Editor): option to minify output
    Fix #1605

--- src/tools/html-wysiwyg-editor/html-wysiwyg-editor.vue
+++ src/tools/html-wysiwyg-editor/html-wysiwyg-editor.vue
@@ -1,16 +1,34 @@
 <script setup lang="ts">
-import { format } from 'prettier';
-import htmlParser from 'prettier/plugins/html';
+import { minify, prettify } from 'htmlfy';
 import { useStorage } from '@vueuse/core';
 import Editor from './editor/editor.vue';
 import TextareaCopyable from '@/components/TextareaCopyable.vue';
+import { useQueryParamOrStorage } from '@/composable/queryParams';
 
 const html = useStorage('html-wysiwyg-editor--html', '<h1>Hey!</h1><p>Welcome to this html wysiwyg editor</p>');
 
-const formattedHtml = asyncComputed(() => format(html.value, { parser: 'html', plugins: [htmlParser] }), '');
+const minifyOutput = useQueryParamOrStorage({ name: 'minify', storageName: 'html-wysiwyg-editor--min', defaultValue: false });
+
+const formattedHtml = computed(() => {
+  try {
+    if (minifyOutput.value) {
+      return minify(html.value);
+    }
+
+    return prettify(html.value);
+  }
+  catch {
+    return html.value;
+  }
+});
 </script>
 
 <template>
   <Editor v-model:html="html" />
+  <NSpace justify="center">
+    <n-form-item label="Minify:" label-placement="left" mt-2>
+      <NSwitch v-model:value="minifyOutput" />
+    </n-form-item>
+  </NSpace>
   <TextareaCopyable :value="formattedHtml" language="html" />
 </template>
