e9dbf1fb0ece43d705f8964484d58950d8781e0b
ShareVB <sharevb@gmail.com>
2025-07-14 20:49:34 +0200

    feat(yaml-to-json): add nestify options

--- src/tools/yaml-to-json-converter/yaml-to-json.vue
+++ src/tools/yaml-to-json-converter/yaml-to-json.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 import { parse as parseYaml } from 'yaml';
+import { nestifyObject } from 'nestify-anything';
 import type { UseValidationRule } from '@/composable/validation';
 import { isNotThrowing } from '@/utils/boolean';
 import { withDefaultOnError } from '@/utils/defaults';
 
+const nestify = ref(false);
+
 function transformer(value: string) {
   return withDefaultOnError(() => {
-    const obj = parseYaml(value, { merge: true });
+    let obj = parseYaml(value, { merge: true });
+    if (nestify.value) {
+      if (obj && Array.isArray(obj)) {
+        obj = (obj as any[]).map(o => nestifyObject(o));
+      }
+      else {
+        obj = nestifyObject(obj);
+      }
+    }
     return obj ? JSON.stringify(obj, null, 3) : '';
   }, '');
 }
@@ -20,6 +31,12 @@ const rules: UseValidationRule<string>[] = [
 </script>
 
 <template>
+  <n-space justify="center">
+    <n-form-item label="Nestify ('a.b.c' to nested objects)" label-placement="left">
+      <n-checkbox v-model:checked="nestify" />
+    </n-form-item>
+  </n-space>
+
   <format-transformer
     input-label="Your YAML"
     input-placeholder="Paste your yaml here..."
