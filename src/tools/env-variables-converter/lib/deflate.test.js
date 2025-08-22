9f70133188b09a156840be8d0c7a1b23cf522f56
ShareVB <sharevb@gmail.com>
2025-08-22 13:31:14 +0200

    fixup! feat(new tool): Env Variables Converter

--- src/tools/env-variables-converter/lib/deflate.test.js
+++ src/tools/env-variables-converter/lib/deflate.test.js
@@ -1,37 +1,39 @@
-import { describe, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import deflate from './deflate';
 
-describe('deflates json', () => {
-  expect(deflate({ 'foo-bar': { baz: [{ biz: ['value1', 'value2'] }] } }, ''))
-    .toStrictEqual(['foo-bar.baz[0].biz[0]=value1', 'foo-bar.baz[0].biz[1]=value2']);
-});
+describe('deflate', () => {
+  it('deflates json', () => {
+    expect(deflate({ 'foo-bar': { baz: [{ biz: ['value1', 'value2'] }] } }, ''))
+      .toStrictEqual(['foo-bar.baz[0].biz[0]=value1', 'foo-bar.baz[0].biz[1]=value2']);
+  });
 
-describe('deflates large json', () => {
-  expect(deflate({
-    cloud: {
-      gateway: {
-        routes: [
-          {
-            id: 'after_route',
-            uri: 'https://example.org',
-            predicates: [
-              {
-                name: 'Cookie',
-                args: {
-                  name: 'mycookie',
-                  regexp: 'mycookievalue',
+  it('deflates large json', () => {
+    expect(deflate({
+      cloud: {
+        gateway: {
+          routes: [
+            {
+              id: 'after_route',
+              uri: 'https://example.org',
+              predicates: [
+                {
+                  name: 'Cookie',
+                  args: {
+                    name: 'mycookie',
+                    regexp: 'mycookievalue',
+                  },
                 },
-              },
-            ],
-          },
-        ],
+              ],
+            },
+          ],
+        },
       },
-    },
-  }, ''))
-    .toStrictEqual(
-      ['cloud.gateway.routes[0].id=after_route',
-        'cloud.gateway.routes[0].uri=https://example.org',
-        'cloud.gateway.routes[0].predicates[0].name=Cookie',
-        'cloud.gateway.routes[0].predicates[0].args.name=mycookie',
-        'cloud.gateway.routes[0].predicates[0].args.regexp=mycookievalue']);
+    }, ''))
+      .toStrictEqual(
+        ['cloud.gateway.routes[0].id=after_route',
+          'cloud.gateway.routes[0].uri=https://example.org',
+          'cloud.gateway.routes[0].predicates[0].name=Cookie',
+          'cloud.gateway.routes[0].predicates[0].args.name=mycookie',
+          'cloud.gateway.routes[0].predicates[0].args.regexp=mycookievalue']);
+  });
 });
